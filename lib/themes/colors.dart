9932fa6552e57a4e54a847ae90855f57f8e74309
Boyani <menechamaryleen@gmail.com>
2025-06-17 18:50:33 +0300

    chore: all colors are defined here

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -5,5 +5,6 @@ class CustomizedColors {
   static const Color textcolor1 = Color(0xFFFFFFFF);
   static const Color textcolor2 = Color(0x00000000);
   static const Color bgcolor = Color(0xFFFFE1F2);
+  static const Color primarycolor = Color(0xFFFF2BA3);
   //button colors
 }
